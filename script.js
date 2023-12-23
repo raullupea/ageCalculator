2d60dc33e53e9ff5a2a39029898fa62a7a6f57e4
raullupea <>
2023-12-23 01:27:44 +0000

    Add variables for Datepicker and variables for printing values

---
+++ script.js
@@ -0,0 +1,13 @@
+// Variables for Datepicker
+
+var datePicker = document.getElementById('datePicker');
+var choseDate = document.getElementById('choseDate');
+
+// Variables for printing Values
+
+var ageYear = document.getElementById('ageYear');
+var ageMonth = document.getElementById('ageMonth');
+var ageDays = document.getElementById('ageDays');
+var ageHours = document.getElementById('ageHours');
+var ageSeconds = document.getElementById('ageSeconds');
+var ageMiliSeconds = document.getElementById('ageMiliSeconds');

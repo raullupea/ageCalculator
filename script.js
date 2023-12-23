1e68507715b1659bf66cac89d9923c84ee870526
raullupea <>
2023-12-23 01:29:41 +0000

    Add event listener to listen any change done in the input area

--- script.js
+++ script.js
@@ -11,3 +11,9 @@ var ageDays = document.getElementById('ageDays');
 var ageHours = document.getElementById('ageHours');
 var ageSeconds = document.getElementById('ageSeconds');
 var ageMiliSeconds = document.getElementById('ageMiliSeconds');
+
+// this event listener listens when box changed and do the function...
+
+datePicker.addEventListener('change', function () {
+    
+}
